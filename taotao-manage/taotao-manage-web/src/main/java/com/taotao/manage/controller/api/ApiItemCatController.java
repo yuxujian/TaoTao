4c1b6507e7c05c8a4f3cfbb6534146c575f05989
XujianYu <yuxujian@gmail.com>
2017-11-08 14:56:15 -0800

    fixed json covers chinese bugs

--- taotao-manage/taotao-manage-web/src/main/java/com/taotao/manage/controller/api/ApiItemCatController.java
+++ taotao-manage/taotao-manage-web/src/main/java/com/taotao/manage/controller/api/ApiItemCatController.java
@@ -1,12 +1,15 @@
 package com.taotao.manage.controller.api;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.taotao.common.bean.ItemCatResult;
 import com.taotao.manage.service.ItemCatService;
 
@@ -16,15 +19,20 @@ public class ApiItemCatController {
 	@Autowired
 	private ItemCatService itemCatService;
 	
+	private static final ObjectMapper MAPPER = new ObjectMapper();
 	/**
 	 * 对外提供接口服务，查询所有类目数据 
 	 * @return
 	 */
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<ItemCatResult> queryItemCatList() {
+	public ResponseEntity<String> queryItemCatList(@RequestParam(value="callback", required = false) String callback) {
 		try {
 			ItemCatResult itemCatResult = this.itemCatService.queryAllToTree();
-			return ResponseEntity.ok(itemCatResult);
+			String json = MAPPER.writeValueAsString(itemCatResult);
+			if(StringUtils.isEmpty(callback)) {
+				return ResponseEntity.ok(json);
+			}
+			return ResponseEntity.ok(callback + "(" + json + ");");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
