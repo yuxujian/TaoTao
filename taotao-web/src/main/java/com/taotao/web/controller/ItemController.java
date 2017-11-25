000d650ffc233ac50074877dc49bbfc2d60d22ee
YuDeskTop <YuDeskTop@DESKTOP-6BT1P6F.attlocal.net>
2017-11-25 12:23:53 -0800

    add Item details Api function for web page

--- taotao-web/src/main/java/com/taotao/web/controller/ItemController.java
+++ taotao-web/src/main/java/com/taotao/web/controller/ItemController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.taotao.manage.pojo.ItemDesc;
 import com.taotao.web.bean.Item;
 import com.taotao.web.service.ItemService;
 
@@ -23,6 +24,15 @@ public class ItemController {
 		//设置模型数据 
 		Item item = this.itemService.queryById(itemId);
 		mv.addObject("item", item);
+		
+		//商品描述数据 
+		ItemDesc itemDesc = this.itemService.queryDescByItemId(itemId);
+		mv.addObject("itemDesc",itemDesc);
+		
+		//商品规格参数数据 
+		String itemParam = this.itemService.queryItemParamItemByItemId(itemId);
+		mv.addObject("itemParam", itemParam);
+		
 		return mv;
 	}
 }
--- taotao-web/src/main/java/com/taotao/web/service/ItemService.java
+++ taotao-web/src/main/java/com/taotao/web/service/ItemService.java
@@ -5,45 +5,106 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.taotao.manage.pojo.ItemDesc;
+import com.taotao.manage.pojo.ItemParamItem;
 import com.taotao.web.bean.Item;
-
 
 @Service
 public class ItemService {
 	@Autowired
 	private ApiService apiService;
-	
+
 	@Value("${TAOTAO_MANAGE_URL}")
 	private String TAOTAO_MANAGE_URL;
-	
-	
+
 	private static final ObjectMapper MAPPER = new ObjectMapper();
-	
-	
+
 	/**
-	 * 根据商品id查询商品数据 
+	 * 根据商品id查询商品数据
 	 * 
 	 * 通过后台系统 提供的接口服务进行查询
+	 * 
 	 * @param itemId
 	 * @return
 	 */
 	public Item queryById(Long itemId) {
 		String url = TAOTAO_MANAGE_URL + "/rest/api/item/" + itemId;
-		
+
 		try {
 			String jsonData = this.apiService.doGet(url);
-			if(StringUtils.isEmpty(jsonData)) {
+			if (StringUtils.isEmpty(jsonData)) {
 				return null;
 			}
-			
-			//将json数据 反序列为Item对象
+
+			// 将json数据 反序列为Item对象
 			return MAPPER.readValue(jsonData, Item.class);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+	
+	/**
+	 * 查询商品描述数据
+	 * @param itemId
+	 * @return
+	 */
+	public ItemDesc queryDescByItemId(Long itemId) {
+		String url = TAOTAO_MANAGE_URL + "/rest/api/item/desc/" + itemId;
+
+		try {
+			String jsonData = this.apiService.doGet(url);
+			if (StringUtils.isEmpty(jsonData)) {
+				return null;
+			}
+
+			// 将json数据 反序列为ItemDesc对象
+			return MAPPER.readValue(jsonData, ItemDesc.class);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public String queryItemParamItemByItemId(Long itemId) {
+		String url = TAOTAO_MANAGE_URL + "/rest/api/item/param/item/" + itemId;
+
+		try {
+			String jsonData = this.apiService.doGet(url);
+			if (StringUtils.isEmpty(jsonData)) {
+				return null;
+			}
+
+			// 将json数据 反序列
+			ItemParamItem itemParamItem =  MAPPER.readValue(jsonData, ItemParamItem.class);
+			String paramData = itemParamItem.getParamData();
+			
+			// 解析JSON
+			
+            ArrayNode arrayNode = (ArrayNode) MAPPER.readTree(paramData);
+            StringBuilder sb = new StringBuilder();
+            sb.append("<table cellpadding=\"0\" cellspacing=\"1\" width=\"100%\" border=\"0\" class=\"Ptable\"><tbody>");
+            for (JsonNode param : arrayNode) {
+                sb.append("<tr><th class=\"tdTitle\" colspan=\"2\">" + param.get("group").asText()
+                        + "</th></tr>");
+                ArrayNode params = (ArrayNode) param.get("params");
+                for (JsonNode p : params) {
+                    sb.append("<tr><td class=\"tdTitle\">" + p.get("k").asText() + "</td><td>"
+                            + p.get("v").asText() + "</td></tr>");
+                }
+            }
+            sb.append("</tbody></table>");
+            return sb.toString();
 			
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 
-		
+		}
 		return null;
 	}
 
